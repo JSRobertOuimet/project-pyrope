f089521fb38b225a91b3a49171b91e1d572df696
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-05-06 13:07:33 -0400

    Centralized messaging; triggered specific  validation by request

---
+++ messaging/messaging.js
@@ -0,0 +1,17 @@
+const messages = {
+  error:  {
+    emailAlreadyUsed: "This email address has already been used.",
+    incorrectEmailOrPassword: "The email address or password is incorrect.",
+    noProfileFound: "No profile found.",
+    noUserFound: "No user found.",
+    usernameAlreadyUsed: "This username has already been used."
+  },
+  success: {
+    createdProfile: "Your profile has been successfully created.",
+    loggedIn: "You have been successfully logged in.",
+    registeredUser: "You have been successfully registered.",
+    updatedProfile: "Your profile has been successfully updated."
+  }
+};
+
+module.exports = messages;
--- validation/validation.js
+++ validation/validation.js
@@ -1,19 +1,46 @@
 const validator = require("validator");
 
-const validate = userInputs => {
+const validate = (userInputs, action) => {
   let errors = {};
 
-  // Email
-  if(!validator.isEmail(userInputs.email)) {
-    errors.email = "Please enter a valid email address.";
-  }
-
-  // Password
-  if(!validator.isLength(userInputs.password, { min: 8 })) {
-    errors.password = "The password must be at least 8 characters long.";
+  switch(action) {
+  case "register":
+    emailValidation(userInputs.email);
+    passwordValidation(userInputs.password);
+    break;
+  case "updateOrCreateProfile":
+    usernameValidation(userInputs.username);
+    aboutValidation(userInputs.about);
+    break;
+  default:
+    console.log("none");
   }
 
   return errors;
+
+  function emailValidation(email) {
+    if(!validator.isEmail(email + "")) {
+      return errors.email = "Please enter a valid email address.";
+    }
+  }
+
+  function passwordValidation(password) {
+    if(!validator.isLength(password + "", { min: 8 })) {
+      return errors.password = "The password must be at least 8 characters long.";
+    }
+  }
+
+  function usernameValidation(username) {
+    if(!validator.isLength(username + "", { min: 2, max: 40 })) {
+      return errors.username = "Your username must be between 2 and 40 characters long.";
+    }
+  }
+
+  function aboutValidation(about) {
+    if(!validator.isLength(about + "", { max: 40 })) {
+      return errors.username = "Your profile’s about section cannot exceed 140 characters.";
+    }
+  }
 };
 
 module.exports = validate;
