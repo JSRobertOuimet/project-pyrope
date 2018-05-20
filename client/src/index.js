f5fa90dd4f8c876c797650c1b4fbf390d21d9c94
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-05-19 22:20:31 -0400

    Idem

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
+
+// Bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/jquery/dist/jquery.min.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
