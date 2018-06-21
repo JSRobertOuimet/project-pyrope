017dbf237133d04c32c6af3cfff84c970800b917
JSRobertOuimet <jouimet@kivuto.com>
2018-06-21 09:57:31 -0400

    Removed Redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    process.env.NODE_ENV === "production" ? null : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore( rootReducer, initialState, applyMiddleware(...middleware));
 
 export default store;
