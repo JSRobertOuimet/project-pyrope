89332ee7ef862d817b4a0a65c126be6b33253565
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-06-24 11:32:53 -0400

    Centered plus icon without flexbox for browsers compatibility

--- client/src/components/common/AddCard.js
+++ client/src/components/common/AddCard.js
@@ -7,8 +7,8 @@ import PropTypes from "prop-types";
 const AddCard = ({ onClick }) => {
   return (
     <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
-      <a className="card bg-light create-challenge d-flex justify-content-center align-items-center" onClick={onClick}>
-        <i className="position-absolute fas fa-plus-circle fa-2x"></i>
+      <a className="card bg-light create-challenge position-relative" onClick={onClick}>
+        <i className="block-center fas fa-plus-circle fa-2x"></i>
       </a>
     </div>
   );
